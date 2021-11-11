c63ae45ad99f7be0a062cab425486cd22a8f7e68
Allen Guan <me@nella17.tw>
2021-11-11 21:15:26 +0800

    Update sparse-table

--- codebook/data-structure/sparse-table.cpp
+++ codebook/data-structure/sparse-table.cpp
@@ -1,14 +1,17 @@
-struct Sparse_table {
-  int st[__lg(MAXN) + 1][MAXN], n;
-  void init(int _n, int *data) {
+template<typename T = int, typename CMP = greater<T>>
+struct SparseTable {
+  int n;
+  T st[__lg(MAXN) + 1][MAXN];
+  CMP cmp;
+  inline T max(T a, T b) { return cmp(a,b) ? a : b; }
+  void init(int _n, auto data) {
     n = _n;
-    for (int i = 0; i < n; ++i) st[i][0] = data[i];
+    for (int i = 0; i < n; ++i) st[0][i] = data[i];
     for (int i = 1, t = 2; t < n; t <<= 1, i++)
       for (int j = 0; j + t <= n; j++)
-        st[i][j] =
-          max(st[i - 1][j], st[i - 1][j + t / 2]);
+        st[i][j] = max(st[i-1][j], st[i-1][j + t/2]);
   }
-  int query(int a, int b) {
+  T query(int a, int b) { // [a,b]
     int t = __lg(b - a + 1);
     return max(st[t][a], st[t][b - (1 << t) + 1]);
   }
