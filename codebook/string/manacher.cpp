e01d199bc896c1adf72ca0a5746c5d3a245efd53
Allen Guan <me@nella17.tw>
2021-11-24 17:08:42 +0800

    Update manacher

--- codebook/string/manacher.cpp
+++ codebook/string/manacher.cpp
@@ -2,17 +2,18 @@ int z[MAXN];
 int Manacher(string tmp) {
   string s = "&";
   int l = 0, r = 0, x, ans;
-  for (char c : tmp) s.pb(c), s.pb('%');
+  for (char c : tmp) s += c, s += '%';
   ans = 0, x = 0;
-  for (int i = 1; i < SZ(s); ++i) {
+  const int sz = s.size();
+  for (int i = 1; i < sz; i++) {
     z[i] = r > i ? min(z[2 * l - i], r - i) : 1;
     while (s[i + z[i]] == s[i - z[i]]) ++z[i];
     if (z[i] + i > r) r = z[i] + i, l = i;
   }
-  for (int i = 1; i < SZ(s); ++i)
+  for (int i = 1; i < sz; i++)
     if (s[i] == '%') x = max(x, z[i]);
   ans = x / 2 * 2, x = 0;
-  for (int i = 1; i < SZ(s); ++i)
+  for (int i = 1; i < sz; i++)
     if (s[i] != '%') x = max(x, z[i]);
   return max(ans, (x - 1) / 2 * 2 + 1);
 }
