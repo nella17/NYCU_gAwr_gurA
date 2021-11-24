f800ac439abc8ee97b6ff8bc578ad0ade03f2f00
Allen Guan <me@nella17.tw>
2021-11-24 19:54:21 +0800

    Update codebook

--- codebook/basic/black-magic.cpp
+++ codebook/basic/black-magic.cpp
@@ -8,9 +8,8 @@ using rkt = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 mt19937 rng((int)chrono::steady_clock::now().time_since_epoch().count());
 template<typename T> T randint(T n) { return uniform_int_distribution<T>(T(0),T(n-1))(rng); }
 int main() {
-  heap h1, h2;
-  h1.push(1), h1.push(3);
-  h2.push(2), h2.push(4);
+  heap h1, h1.push(1), h1.push(3);
+  heap h2; h2.push(2), h2.push(4);
   h1.join(h2);
   cout _ h1.size() _ h2.size() _ h1.top(); //4 0 4
   rkt<int> st; for (int x : {0, 2, 3, 4}) st.insert(x);
--- codebook/geometry/df.cpp
+++ codebook/geometry/df.cpp
@@ -4,16 +4,16 @@ using Vt = Pt;
 using Line = pair<Pt,Pt>;
 const double eps = 1e-9;
 bool is_zero(Dt x) { return -eps < x and x < eps; }
-Pt operator+(Pt a, Pt b) { return Pt{ a.ft+b.ft, a.sd+b.sd }; }
-Pt operator-(Pt a, Pt b) { return Pt{ a.ft-b.ft, a.sd-b.sd }; }
-Pt operator*(Pt a, Dt k) { return Pt{ a.ft*k, b.sd*k }; }
-Pt operator/(Pt a, Dt k) { return Pt{ a.ft/k, b.sd/k }; }
+Pt operator+(Pt a,Pt b) { return Pt{ a.ft+b.ft,a.sd+b.sd }; }
+Pt operator-(Pt a,Pt b) { return Pt{ a.ft-b.ft,a.sd-b.sd }; }
+Pt operator*(Pt a,Dt k) { return Pt{ a.ft*k,b.sd*k }; }
+Pt operator/(Pt a,Dt k) { return Pt{ a.ft/k,b.sd/k }; }
 Dt dot(Vt a, Vt b) { return a.ft*b.ft + a.sd*b.sd; }
 Dt cross(Vt a, Vt b) { return a.ft*b.sd - a.sd*b.ft; }
 Dt abs2(Vt a) { return dot(a,a); }
 ld abs(Vt a) { return sqrt(dot(a,a));}
 int sign(Dt x) { return is_zero(x) ? 0 : x > 0 ? 1 : -1; }
-int ori(Pt a, Pt b, Pt c) { return sign(cross(b - a, c - a));}
+int ori(Pt a, Pt b, Pt c) { return sign(cross(b-a, c-a));}
 bool collinearity(Pt p1, Pt p2, Pt p3) { return is_zero(cross(p1 - p3, p2 - p3)); }
 bool btw(Pt p1, Pt p2, Pt p3) {
   if(!collinearity(p1, p2, p3)) return 0;
