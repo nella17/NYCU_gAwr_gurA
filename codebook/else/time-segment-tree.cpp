b71c7b7c90956d57c5a32de24e05a1ca25625dce
Allen Guan <me@nella17.tw>
2021-11-24 18:17:24 +0800

    Update time segment tree

--- codebook/else/time-segment-tree.cpp
+++ codebook/else/time-segment-tree.cpp
@@ -1,18 +1,13 @@
 vector<Event> tr[MAXT << 2];
 void insert_event(int o, int l, int r, int ql, int qr, Event e) {
-    if (ql <= l && r <= qr)
+    if (r <= ql or  qr <= l) return;
+    if (ql <= l and r <= qr)
         return tr[o].push_back(e), void();
     int m = l + (r - l) / 2;
     int y = o+1;
     int z = o + (r - l) / 2 * 2;
-    if (qr < m)
-        insert_event(y, l, m, ql, qr, e);
-    else if (m <= ql)
-        insert_event(z, m, r, ql, qr, e);
-    else {
-        insert_event(y, l, m, ql, qr, e);
-        insert_event(z, m, r, ql, qr, e);
-    }
+    insert_event(y, l, m, ql, qr, e);
+    insert_event(z, m, r, ql, qr, e);
 }
 void traversal(int o, int l, int r) {
     int cnt = 0;
