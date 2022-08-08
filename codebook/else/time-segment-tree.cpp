627115a6056abaca7d73936a1ec15b3d693ac5b3
Allen Guan <me@nella17.tw>
2022-08-08 21:43:37 +0800

    short time-segment-tree

--- codebook/else/time-segment-tree.cpp
+++ codebook/else/time-segment-tree.cpp
@@ -1,11 +1,11 @@
-vector<Event> tr[MAXT << 2];
+vector<Event> tr[MAXT << 1];
+#define MYZ int m = l + (r - l) / 2, \
+    y = o + 1, z = o + (r - l) / 2 * 2
 void insert_event(int o, int l, int r, int ql, int qr, Event e) {
     if (r <= ql or  qr <= l) return;
     if (ql <= l and r <= qr)
         return tr[o].push_back(e), void();
-    int m = l + (r - l) / 2;
-    int y = o+1;
-    int z = o + (r - l) / 2 * 2;
+    MYZ;
     insert_event(y, l, m, ql, qr, e);
     insert_event(z, m, r, ql, qr, e);
 }
@@ -16,9 +16,7 @@ void traversal(int o, int l, int r) {
             cnt++;
     if (l == r-1) // record ans
     else {
-        int m = l + (r - l) / 2;
-        int y = o+1;
-        int z = o + (r - l) / 2 * 2;
+        MYZ;
         traversal(y, l, m);
         traversal(z, m, r);
     }
