208c4eef1c1387392790aa927bf1c0e7575ef26b
Allen Guan <me@nella17.tw>
2022-08-08 21:23:14 +0800

    update mos algorithm

--- codebook/else/mos-alogrithm-with-modification.cpp
+++ codebook/else/mos-alogrithm-with-modification.cpp
@@ -1,29 +1,22 @@
-struct QUERY { // BLOCK=N^{2/3}
-  int L,R,id,LBid,RBid,T;
-  QUERY(int l,int r,int id,int lb,int rb,int t):
-    L(l),R(r),id(id),LBid(lb),RBid(rb),T(t){}
-  bool operator<(const QUERY &b)const{
-    if(LBid!=b.LBid) return LBid<b.LBid;
-    if(RBid!=b.RBid) return RBid<b.RBid;
-    return T<b.T;
-  }
+struct Query { // BLOCK = N^{2/3}
+  int id, L, R, Li, Ri, T;
+  Query(int i, int l, int r, int t):
+    id(i), L(l), R(r), Li(l/BLOCK), Ri(r/BLOCK), T(t){}
+  bool operator<(const Query &b) const {
+    return tuple{ Li,Ri,T } < tuple{ b.Li,b.Ri,b.T }; }
 };
-vector<QUERY> query;
-int cur_ans,arr[MAXN],ans[MAXN];
-void addTime(int L,int R,int T){}
-void subTime(int L,int R,int T){}
-void add(int x){}
-void sub(int x){}
-void solve(){
+vector<Query> query;
+int cur_ans, arr[MAXN], ans[MAXQ];
+void solve() {
   sort(ALL(query));
-  int L=0,R=0,T=-1;
-  for(auto q:query){
-    while(T<q.T) addTime(L,R,++T);
-    while(T>q.T) subTime(L,R,T--);
-    while(R<q.R) add(arr[++R]);
-    while(L>q.L) add(arr[--L]);
-    while(R>q.R) sub(arr[R--]);
-    while(L<q.L) sub(arr[L++]);
-    ans[q.id]=cur_ans;
+  int L = 0, R = -1, T = -1;
+  for(auto q: query) {
+    while(T < q.T) addTime(L,R,++T); // TODO
+    while(T > q.T) subTime(L,R,T--); // TODO
+    while(R < q.R) add(arr[++R]); // TODO
+    while(L > q.L) add(arr[--L]); // TODO
+    while(R > q.R) sub(arr[R--]); // TODO
+    while(L < q.L) sub(arr[L++]); // TODO
+    ans[q.id] = cur_ans;
   }
 }
