b9e449bf6b290b370adb94001a54199823473446
Allen Guan <me@nella17.tw>
2022-08-21 11:25:35 +0800

    finish 101635-K, add comment to convex-hull

--- codebook/geometry/convex-hull.cpp
+++ codebook/geometry/convex-hull.cpp
@@ -2,11 +2,11 @@ void convex_hull(vector<Pt> &dots) {
   sort(dots.begin(), dots.end());
   vector<Pt> A(1, dots[0]);
   const int sz = dots.size()
-  for(int ct = 0; ct < 2; reverse(ALL(dots)), ++ct)
+  for(int c = 0; c < 2; reverse(ALL(dots)), c++)
     for(int i = 1, t = A.size(); i < sz;
             A.emplace_back(dots[i++]))
       while (A.size() > t and
           ori(A[A.size()-2], A.back(), dots[i]) <= 0)
         A.pop_back();
   A.pop_back(), A.swap(dots);
-}
+} // dots.size() changed !!!
