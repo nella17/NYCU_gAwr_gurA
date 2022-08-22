dab59997bc4c5f455df45d0a39686afacb4a680a
Allen Guan <me@nella17.tw>
2022-08-22 10:15:00 +0800

    fix KM

--- codebook/flow/km.cpp
+++ codebook/flow/km.cpp
@@ -5,18 +5,18 @@ struct KM { // 0-base
   void init(int _n) {
     n = _n;
     for (int i = 0; i < n; ++i)
-      for (int j = 0; j < n; ++j) w[i][j] = -INF;
+      for (int j = 0; j < n; ++j) w[i][j] = 0;// TODO
   }
   void add_edge(int a, int b, int wei) {
     w[a][b] = wei;
   }
-  bool Check(int x) {
+  bool check(int x) {
     if (vl[x] = 1, ~fl[x])
       return vr[qu[qr++] = fl[x]] = 1;
     while (~x) swap(x, fr[fl[x] = pre[x]]);
     return 0;
   }
-  void Bfs(int s) {
+  void bfs(int s) {
     fill(slk, slk + n, INF);
     fill(vl, vl + n, 0), fill(vr, vr + n, 0);
     ql = qr = 0, qu[qr++] = s, vr[s] = 1;
@@ -27,7 +27,7 @@ struct KM { // 0-base
           if (!vl[x] &&
             slk[x] >= (d = hl[x] + hr[y] - w[x][y]))
             if (pre[x] = y, d) slk[x] = d;
-            else if (!Check(x)) return;
+            else if (!check(x)) return;
       d = INF;
       for (int x = 0; x < n; ++x)
         if (!vl[x] && d > slk[x]) d = slk[x];
@@ -37,15 +37,14 @@ struct KM { // 0-base
         if (vr[x]) hr[x] -= d;
       }
       for (int x = 0; x < n; ++x)
-        if (!vl[x] && !slk[x] && !Check(x)) return;
+        if (!vl[x] && !slk[x] && !check(x)) return;
     }
   }
-  int Solve() {
-    fill(fl, fl + n, -1), fill(fr, fr + n, -1),
-      fill(hr, hr + n, 0);
+  int solve() {
+    fill(fl,fl+n,-1),fill(fr,fr+n,-1),fill(hr,hr+n,0);
     for (int i = 0; i < n; ++i)
       hl[i] = *max_element(w[i], w[i] + n);
-    for (int i = 0; i < n; ++i) Bfs(i);
+    for (int i = 0; i < n; ++i) bfs(i);
     int res = 0;
     for (int i = 0; i < n; ++i) res += w[i][fl[i]];
     return res;
