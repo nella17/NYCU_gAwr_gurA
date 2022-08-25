2ee3bc64bcc527aa8fcc0b756e34c2a82d7ad3c8
Allen Guan <me@nella17.tw>
2022-08-26 04:06:03 +0800

    add extra optimize

--- codebook/basic/df.cpp
+++ codebook/basic/df.cpp
@@ -1,5 +1,6 @@
 #pragma GCC optimize("Ofast,unroll-loops,fast-math")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+// #pragma GCC ivdep // before loop
 #include<bits/stdc++.h>
 using namespace std;
 #ifndef DEBUG
