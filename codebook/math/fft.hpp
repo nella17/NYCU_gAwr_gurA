f166c658b8e476deb08f80fc8d64556ce491e558
Allen Guan <me@nella17.tw>
2022-09-05 20:28:03 +0800

    add fft & ntt from others
    https://github.com/ToxicPie/NaCl/tree/964476f71e24139911123ea7be214813ac5e4be8/nacl/numeric
    https://github.com/brianbbsu/8BQube/blob/master/codebook/7_Polynomial/Number_Theory_Transform.cpp

---
+++ codebook/math/fft.hpp
@@ -0,0 +1,36 @@
+template <typename T>
+void fft_(int n,vector<T> &a,vector<T> &rt,bool inv) {
+  vector<int> br(n);
+  for (int i = 1; i < n; i++) {
+    br[i] = (i & 1) ? br[i - 1] + n / 2 : br[i / 2] / 2;
+    if (br[i] > i) swap(a[i], a[br[i]]);
+  }
+  for (int len = 2; len <= n; len *= 2)
+    for (int i = 0; i < n; i += len)
+      for (int j = 0; j < len / 2; j++) {
+        int pos = n / len * (inv ? len - j : j);
+        T u = a[i + j], v = a[i + j + len/2] * rt[pos];
+        a[i + j] = u + v, a[i + j + len/2] = u - v;
+      }
+  if (T minv = T(1) / T(n); inv)
+    for (T &x : a) x *= minv;
+}
+void fft(vector<complex<double>> &a, bool inv) {
+  int n = a.size();
+  vector<complex<double>> rt(n + 1);
+  double arg = acos(-1) * 2 / n;
+  for (int i = 0; i <= n; i++)
+    rt[i] = {cos(arg * i), sin(arg * i)};
+  fft_(n, a, rt, inv);
+}
+// (2^16)+1, 65537, 3
+// 7*17*(2^23)+1, 998244353, 3
+// 1255*(2^20)+1, 1315962881, 3
+// 51*(2^25)+1, 1711276033, 29
+void ntt(vector<Mod> &a, bool inv, Mod primitive_root){
+  int n = a.size();
+  Mod root = primitive_root ^ (MOD - 1) / n;
+  vector<Mod> rt(n + 1, 1);
+  for (int i = 0; i < n; i++) rt[i + 1] = rt[i] * root;
+  fft_(n, a, rt, inv);
+}
