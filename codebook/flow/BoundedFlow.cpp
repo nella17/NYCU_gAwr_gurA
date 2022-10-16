aae384f117b86527239553d7a70c9671355c4780
Allen Guan <me@nella17.tw>
2022-10-16 16:52:09 +0800

    update boundedflow

--- codebook/flow/BoundedFlow.cpp
+++ codebook/flow/BoundedFlow.cpp
@@ -2,8 +2,8 @@ struct BoundedFlow { // 0-base
   struct edge {
     int to, cap, flow, rev;
   };
-  vector<edge> G[N];
-  int n, s, t, dis[N], cur[N], cnt[N];
+  vector<edge> G[MAXM];
+  int n, s, t, dis[MAXM], cur[MAXM], cnt[MAXM];
   void init(int _n) {
     n = _n;
     for (int i = 0; i < n + 2; ++i)
@@ -11,18 +11,16 @@ struct BoundedFlow { // 0-base
   }
   void add_edge(int u, int v, int lcap, int rcap) {
     cnt[u] -= lcap, cnt[v] += lcap;
-    G[u].pb(edge{v, rcap, lcap, SZ(G[v])});
-    G[v].pb(edge{u, 0, 0, SZ(G[u]) - 1});
+    G[u].eb(edge{v, rcap, lcap, G[v].size() });
+    G[v].eb(edge{u, 0, 0, G[u].size() - 1});
   }
   void add_edge(int u, int v, int cap) {
-    G[u].pb(edge{v, cap, 0, SZ(G[v])});
-    G[v].pb(edge{u, 0, 0, SZ(G[u]) - 1});
-  }
+    add_edge(u, v, 0, cap); }
   int dfs(int u, int cap) {
-    if (u == t || !cap) return cap;
-    for (int &i = cur[u]; i < SZ(G[u]); ++i) {
+    if (u == t or !cap) return cap;
+    for (int &i = cur[u]; i < G[u].size(); i++) {
       edge &e = G[u][i];
-      if (dis[e.to] == dis[u] + 1 && e.cap != e.flow) {
+      if (dis[e.to] == dis[u]+1 and e.cap != e.flow) {
         int df = dfs(e.to, min(e.cap - e.flow, cap));
         if (df) {
           e.flow += df, G[e.to][e.rev].flow -= df;
@@ -37,11 +35,10 @@ struct BoundedFlow { // 0-base
     fill_n(dis, n + 3, -1);
     queue<int> q;
     q.push(s), dis[s] = 0;
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      for (edge &e : G[u])
-        if (!~dis[e.to] && e.flow != e.cap)
+    while (q.size()) {
+      int u = q.front(); q.pop();
+      for (auto &e : G[u])
+        if (dis[e.to] == -1 and e.flow != e.cap)
           q.push(e.to), dis[e.to] = dis[u] + 1;
     }
     return dis[t] != -1;
