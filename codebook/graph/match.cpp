facc4983d0c1abb1276d91efbc0f0817c5e7b621
Allen Guan <me@nella17.tw>
2022-10-20 00:28:31 +0800

    restyle match

--- codebook/graph/match.cpp
+++ codebook/graph/match.cpp
@@ -3,19 +3,19 @@ array<bool,SZ> vis;
 bool dfs(int now) {
     if(vis[now]) return false;
     vis[now] = true;
-    for(int i = 0 ; i < n ; ++i) {
-        if(!g[now][i]) continue;
+    for(int i = 0; i < n; i++) {
+        if(!G[now][i]) continue;
         if(mp[i] == -1 or dfs(mp[i]))
-            return mp[i] = now , 1;
+            return mp[i] = now , true;
     }
     return false;
 }
 int solve() {
     mp.fill(-1);
-    int ret = 0 ;
-    for(int i = 0 ; i < n ; ++i) {
+    int r = 0;
+    for(int i = 0; i < n; i++) {
         vis.fill(false);
-        if(dfs(i)) ++ret;
+        if(dfs(i)) r++;
     }
-    return ret;
+    return r;
 }
