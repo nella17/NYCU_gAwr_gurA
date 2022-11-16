9bbcf8a71be66b66b78b28e8a1a139c8bf50838d
iris2617 <53460413+iris2617@users.noreply.github.com>
2022-11-17 00:21:35 +0800

    Update pollard_rho.cpp

--- codebook/math/pollard_rho.cpp
+++ codebook/math/pollard_rho.cpp
@@ -1,15 +1,15 @@
-ll f(ll x, ll m) { return ll((LL(x) * x + 1) % m); }
-ll pollard_rho(ll n) {
-    if (!(n & 1)) return 2;
-    for(;;) {
-        ll y = 2, x = 1 + randint(n-1), res = 1;
-        for(ll sz = 2; res == 1; sz *= 2) {
-            for(ll i = 0; i < sz and res <= 1; i++) {
-                x = f(x, n);
-                res = __gcd(abs(x - y), n);
-            }
-            y = x;
-        }
-        if (res != 0 and res != n) return res;
+ll pollard_rho(ll x) {
+  ll s = 0, t = 0, v = q, c = randint(1LL, x);
+  for (int g = 1; ; g *= 2, s = t, v = 1) {
+    for (int ss = 1; ss <= g; ++ss) {
+      t = ((LL)t * t + c) % x;
+      val = (LL)v * abs(t - s) % x;
+      if ((ss % 127) == 0) {
+        ll d = __gcd(val, x);
+        if (d > 1) return d;
+      }
     }
+    ll d = __gcd(v, x);
+    if (d > 1) return d;
+  }
 }
