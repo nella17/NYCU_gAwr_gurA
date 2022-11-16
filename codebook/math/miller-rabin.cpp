2d735cc8138e1016525488bd06274033de7678c5
iris2617 <53460413+iris2617@users.noreply.github.com>
2022-11-17 00:16:01 +0800

    Update miller-rabin.cpp

--- codebook/math/miller-rabin.cpp
+++ codebook/math/miller-rabin.cpp
@@ -3,15 +3,15 @@
 // n < 3,474,749,660,383  6 : pirmes <= 13
 // n < 2^64             7 :
 // 2, 325, 9375, 28178, 450775, 9780504, 1795265022
-bool Miller_Rabin(ll a, ll n) {
-  if((a = a % n) == 0) return 1;
-  if((n & 1) ^ 1) return n == 2;
-  ll tmp = (n - 1) / ((n - 1) & (1 - n));
-  ll t = __lg(((n - 1) & (1 - n))), x = 1;
-  for(; tmp; tmp >>= 1, a = mul(a, a, n))
-    if(tmp & 1) x = mul(x, a, n);
-  if(x == 1 || x == n - 1) return 1;
-  while(--t)
-    if((x = mul(x, x, n)) == n - 1) return 1;
-  return 0;
+bool is_prime(ll n) { // ll tns[]
+    if (n < 2 or n%2 == 0) return n == 2;
+    int s = __builtin_ctzll(n-1);
+    for(auto a: tns) {
+        auto x = fpow(a, n >> s, n);
+        int i = 0;
+        while (i < s and (x+1)%n > 2)
+            x = ll(LL(x) * x % n), i++;
+        if (i and x != n-1) return false;
+    }
+    return true;
 }
