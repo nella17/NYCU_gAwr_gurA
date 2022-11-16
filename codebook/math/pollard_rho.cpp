0869570ac40f6290977725e03d294afd6bede548
Allen Guan <me@nella17.tw>
2022-11-17 00:27:25 +0800

    fix pollard_rho

--- codebook/math/pollard_rho.cpp
+++ codebook/math/pollard_rho.cpp
@@ -1,11 +1,11 @@
 ll pollard_rho(ll x) {
-  ll s = 0, t = 0, v = q, c = randint(1LL, x);
+  ll s = 0, t = 0, v = 1, c = randint(1LL, x);
   for (int g = 1; ; g *= 2, s = t, v = 1) {
     for (int ss = 1; ss <= g; ++ss) {
       t = ((LL)t * t + c) % x;
-      val = (LL)v * abs(t - s) % x;
+      v = (LL)v * abs(t - s) % x;
       if ((ss % 127) == 0) {
-        ll d = __gcd(val, x);
+        ll d = __gcd(v, x);
         if (d > 1) return d;
       }
     }
