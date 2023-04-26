731b855883874102a06ba0fb3676fb9cc28629ce
Allen Guan <me@nella17.tw>
2023-04-27 01:35:58 +0800

    fix & refactor: lca

--- codebook/graph/lca.cpp
+++ codebook/graph/lca.cpp
@@ -1,19 +1,17 @@
-int lgN = __lg(n)+1;
-vector<vector<int>> lca_table(n,vector<int>(lgN));
-{   auto &v = lca_table;
+int lgN = __lg(n)+1; // dfs: dep[i], pars[i][0]
+vector<vector<int>> pars(n,vector<int>(lgN));
+for(int d = 1; d < lgN; d++) // pars[0][0] = 0
     for(int i = 0; i < n; i++)
-        v[i][0] = par[i];
-    for(int d = 1; d < lgN; d++)
-        for(int i = 0; i < n; i++)
-            v[i][d] = v[v[i][d-1]][d-1]; }
+        pars[i][d] = pars[pars[i][d-1]][d-1];
+auto gopar = [&](int x, int w) {
+    for (int d = lgN-1; d >= 0; d--)
+        if (w >= (1<<d)) x = pars[x][d], w -= (1<<d);
+    return x; };
 auto lca = [&](int u, int v) {
     if (dep[u] < dep[v]) swap(u,v);
+    u = gopar(u, dep[u] - dep[v]);
+    if (u == v) return u;
     for(int d = lgN-1; d >= 0; d--)
-        if (dep[u]-d > dep[v])
-            u = lca_table[u][d];
-    for(int d = lgN-1; d >= 0; d--)
-        if (lca_table[u][d] != lca_table[v][d])
-            u = lca_table[u][d], v = lca_table[v][d];
-    if (u != v) u = par[u];
-    return u;
-};
+        if (pars[u][d] != pars[v][d])
+            u = pars[u][d], v = pars[v][d];
+    return pars[u][0]; };
