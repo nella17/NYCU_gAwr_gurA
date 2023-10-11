ac303f31901313426091680a83224eb4aad090a9
iris2617 <iris002617@gmail.com>
2023-10-12 01:49:55 +0800

    codebook: add simplex.cpp

---
+++ codebook/math/simplex.cpp
@@ -0,0 +1,63 @@
+const int MAXN = 11000, MAXM = 405;
+const double eps = 1E-10;
+double a[MAXN][MAXM], b[MAXN], c[MAXM];
+double d[MAXN][MAXM], x[MAXM];
+int ix[MAXN + MAXM]; // !!! array all indexed from 0
+// max{cx} subject to {Ax<=b,x>=0}
+// n: constraints, m: vars !!!
+// x[] is the optimal solution vector
+// usage : 
+// value = simplex(a, b, c, N, M);
+double simplex(int n, int m){
+  ++m;
+  fill_n(d[n], m + 1, 0);
+  fill_n(d[n + 1], m + 1, 0);
+  iota(ix, ix + n + m, 0);
+  int r = n, s = m - 1;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m - 1; ++j) d[i][j] = -a[i][j];
+    d[i][m - 1] = 1;
+    d[i][m] = b[i];
+    if (d[r][m] > d[i][m]) r = i;
+  }
+  copy_n(c, m - 1, d[n]);
+  d[n + 1][m - 1] = -1;
+  for (double dd;; ) {
+    if (r < n) {
+      swap(ix[s], ix[r + m]);
+      d[r][s] = 1.0 / d[r][s];
+      for (int j = 0; j <= m; ++j)
+        if (j != s) d[r][j] *= -d[r][s];
+      for (int i = 0; i <= n + 1; ++i) if (i != r) {
+        for (int j = 0; j <= m; ++j) if (j != s)
+          d[i][j] += d[r][j] * d[i][s];
+        d[i][s] *= d[r][s];
+      }
+    }
+    r = s = -1;
+    for (int j = 0; j < m; ++j)
+      if (s < 0 or ix[s] > ix[j]) {
+        if (d[n + 1][j] > eps or
+            (d[n + 1][j] > -eps and d[n][j] > eps))
+          s = j;
+      }
+    if (s < 0) break;
+    for (int i = 0; i < n; ++i) if (d[i][s] < -eps) {
+      if (r < 0 or
+          (dd = d[r][m] / d[r][s] - d[i][m] / d[i][s]) < -eps or
+          (dd < eps and ix[r + m] > ix[i + m]))
+        r = i;
+    }
+    if (r < 0) return -1; // not bounded
+  }
+  if (d[n + 1][m] < -eps) return -1; // not executable
+  double ans = 0;
+  fill_n(x, m, 0);
+  for (int i = m; i < n + m; ++i) { // the missing enumerated x[i] = 0
+    if (ix[i] < m - 1){
+      ans += d[i - m][m] * c[ix[i]];
+      x[ix[i]] = d[i-m][m];
+    }
+  }
+  return ans; 
+}
