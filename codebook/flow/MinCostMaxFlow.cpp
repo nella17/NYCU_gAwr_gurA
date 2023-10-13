cd95e241b05c5baf532d2c71d6129143ef71734e
Allen Guan <me@nella17.tw>
2023-10-13 22:34:47 +0800

    codebook: update MinCostMaxFlow

--- codebook/flow/MinCostMaxFlow.cpp
+++ codebook/flow/MinCostMaxFlow.cpp
@@ -1,17 +1,20 @@
+template<int MAXV, typename T = ll, T INF = LLONG_MAX>
 struct MCMF { // 0-base
   struct edge {
-    ll from, to, cap, flow, cost, rev;
-  } * past[MAXN];
-  vector<edge> G[MAXN];
-  bitset<MAXN> inq;
-  ll dis[MAXN], up[MAXN], s, t, mx, n;
-  bool BellmanFord(ll &flow, ll &cost) {
-    fill(dis, dis + n, INF);
-    queue<ll> q;
+    int from, to, rev;
+    T cap, flow, cost;
+  } * past[MAXV];
+  vector<edge> G[MAXV];
+  bitset<MAXV> inq;
+  int s, t, n;
+  T mx, flow, cost, dis[MAXV], up[MAXV];
+  bool BellmanFord() {
+    fill_n(dis, n, INF);
+    queue<int> q;
     q.push(s), inq.reset(), inq[s] = 1;
     up[s] = mx - flow, past[s] = 0, dis[s] = 0;
     while (!q.empty()) {
-      ll u = q.front(); q.pop(), inq[u] = 0;
+      int u = q.front(); q.pop(), inq[u] = 0;
       if (!up[u]) continue;
       for (auto &e : G[u])
         if (e.flow != e.cap and
@@ -23,24 +26,23 @@ struct MCMF { // 0-base
     }
     if (dis[t] == INF) return 0;
     flow += up[t], cost += up[t] * dis[t];
-    for (ll i = t; past[i]; i = past[i]->from) {
+    for (int i = t; past[i]; i = past[i]->from) {
       auto &e = *past[i];
       e.flow += up[t], G[e.to][e.rev].flow -= up[t];
     }
     return 1;
   }
-  ll MinCostMaxFlow(ll _s, ll _t, ll &cost) {
-    s = _s, t = _t, cost = 0;
-    ll flow = 0;
-    while (BellmanFord(flow, cost)) ;
-    return flow;
+  auto solve(int _s, int _t) {
+    s = _s, t = _t, cost = 0, flow = 0;
+    while (BellmanFord()) ;
+    return pair{ flow, cost };
   }
-  void init(ll _n, ll _mx = INF) {
+  void init(int _n, T _mx = INF) {
     n = _n, mx = _mx;
     for (int i = 0; i < n; ++i) G[i].clear();
   }
-  void add_edge(ll a, ll b, ll cap, ll cost) {
-    G[a].eb(edge{ a, b, cap, 0, cost, G[b].size() });
-    G[b].eb(edge{ b, a, 0, 0, -cost, G[a].size()-1 });
+  void add_edge(int a, int b, T cap, T c) {
+    G[a].eb(edge{ a, b, G[b].size(), cap, 0, c });
+    G[b].eb(edge{ b, a, G[a].size()-1, 0, 0, -c });
   }
 };
